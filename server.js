97cbba1db046d7d7f57d9ff08cb9f30b119f3ff5
Andrew Gould <andrew.scott.gould@gmail.com>
2023-02-16 21:26:23 -0500

    add cors to angular server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var cors = require('cors')
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/tavis'));
+app.use(cors());
 
 app.get('/*', function(req,res) {
     
